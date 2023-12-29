9498de71d66a94ae016d6757b6eec2c6ead72dbe
chasespigarelli21 <cspigarelli9@gmail.com>
2023-12-29 02:10:56 -0700

    erroring when displaying array

--- TheLearningProject/DriverProgram.cpp
+++ TheLearningProject/DriverProgram.cpp
@@ -2,6 +2,12 @@
 
 #include <iostream>
 
+const int STD_STR_LEN = 20;
+
+const int STD_ARRAY_LEN = 20;
+
+const char NEW_LINE_CHAR = '\n';
+
 
 // Function: Take in an array of integers and sort them and return the sorted array
 void SortArray()
@@ -51,9 +57,83 @@ struct car {
 // Create a class 
 
 
+
+
+
 int main()
 {
 
+	// create an integer
+	int myInteger;
+
+	//create a string
+	char myString[STD_STR_LEN];
+
+	// create an array
+	int myArray[STD_ARRAY_LEN];
+
+	int customLength;
+
+	// index used to in the for-loop
+	int index;
+
+	// ask the user for a number of integers to size the array
+	printf("Please enter the number of digits you would like to put in your array: ");
+	scanf_s("%d", &customLength);
+
+	printf("%c", NEW_LINE_CHAR);
+
+	printf("Your custom length is: ");
+	printf("%d", customLength);
+
+	printf("%c", NEW_LINE_CHAR);
+
+	// dynamically allocate memory at runtime
+	int* customArray = new int[customLength];
+
+
+
+
+	// itterate thru the array
+	for (index = 1; index <= customLength; index++)
+	{
+		// ask the user for the digits they would like to put into the array
+		printf("Please enter digit #");
+		printf("%d", index);
+		printf(": ");
+		scanf_s("%d", &customArray[ index ]);
+		printf("%c", NEW_LINE_CHAR);
+	} 
+
+	// sort the array
+
+
+	// display array
+	for (index = 1; index <= customLength; index++)
+	{
+		printf("Displaying Array");
+		printf("%c", NEW_LINE_CHAR);
+		printf("================");
+		printf("%c", NEW_LINE_CHAR);
+
+		printf("Digit #");
+		printf("%d", index);
+		printf(" with value: ");
+		printf("%d", customArray[index]);
+	}
+
+
+
+	
+
+
+
+
+
+
+
+	// free any allocated memory
+	delete[] customArray;
 	
 
 	printf("Program successfully terminated.\n");
