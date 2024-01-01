25eb6064dae773729d327b1f5312b687e5dd6007
chasespigarelli21 <78243585+chasespigarelli@users.noreply.github.com>
2023-12-31 18:46:26 -0700

    organized

--- TheLearningProject/Driver.cpp
+++ TheLearningProject/Driver.cpp
@@ -11,12 +11,6 @@
 
 
 
-
-
-
-
-
-
 int main()
 {
 	// initialize variables
@@ -31,10 +25,19 @@ int main()
 		Node* node = nullptr;
 
 		// prompt for selecting which data structure to work with
-		int prompt;
+		int userSelection;
 
 		// size of linked list
 		int linkedListSize;
+
+		// integer for array prompt
+		const int arraySelection = 1;
+
+		// integer for linked list prompt
+		const int linkedListSelection = 2;
+
+		// used for a for-loop
+		int index;
 
 		
 
@@ -45,73 +48,64 @@ int main()
 	printf("Welcome to the Learning Project.\n");
 	
 
-	printf("Would you like to work with arrays(1) or linked lists(2)?\n");
-	scanf_s("%d", &prompt);
+	printf("Would you like to work with arrays(1) or linked lists(2)? \n");
+	scanf_s("%d", &userSelection);
+
+	// Task #1 --> Array manipulation
+
+	    // If the user select the array manipulation prompt
+		if (userSelection == arraySelection)
+		{
+
+		
+				// Task 1.1: Get array length from user
+			        
+                    // use the function PromptArrayLength() to prompt the user for how long they would like their array to be
+					arrayLength = PromptArrayLength();
+
+					// Assign the array length to the array
+					array = new int[arrayLength];
 
 
 
-	if (prompt == 1)
-	{
+				// Task 1.2: Get digits from user
 
-		// Task #1 --> Array manipulation
-
-
-			// Task 1.1: Get array length from user
-
-			arrayLength = PromptArrayLength();
-
-			// Assign the array length to the array
-			array = new int[arrayLength];
+					// Prompt user for digits in array
+					array = GetDigitsForArray(arrayLength, array);
 
 
 
+				// Task 1.3: Display the array
 
-			// Task 1.2: Get digits from user
-
-				// Prompt user for digits in array
-			array = GetDigitsForArray(arrayLength, array);
-
+					// call function using an array and the array length to display the array
+					DisplayArray(array, arrayLength);
 
 
-			// Task 1.3: Display the array
+				// Task 1.4: Sort the Array
 
-				// call function using an array and the array length to display the array
-			DisplayArray(array, arrayLength);
+					// call function that takes in an array pointer and return the sorted array
+					array = SortArray(array, arrayLength);
 
-
-			// Task 1.4: Sort the Array
-
-				// call function that takes in an array pointer and return the sorted array
-			array = SortArray(array, arrayLength);
-
-			// display the sorted array
-			DisplayArray(array, arrayLength);
+					// display the sorted array
+					DisplayArray(array, arrayLength);
 
 
-			// Task 1.5: Display the array address
+				// Task 1.5: Display the array address
 
-				// call function to display the address of the array
-			DisplayArrayAddress(array);
-
-
-			// Task 1.6: Display the address of each integer in the array
-
-				// call function to display the address of each integer in the array
-			DisplayArrayIntegerAddresses(array, arrayLength);
+					// call function to display the address of the array
+					DisplayArrayAddress(array);
 
 
+				// Task 1.6: Display the address of each integer in the array
 
-			// free array allocated memory
-			delete[] array;
-
-
-
+					// call function to display the address of each integer in the array
+					DisplayArrayIntegerAddresses(array, arrayLength);
 
 
+				// free array allocated memory
+				delete[] array;
 
-
-
-	}
+		}
 
 
 
@@ -121,21 +115,38 @@ int main()
 
 	// Task 2 --> Linked List Manipulation
 
-	if (prompt == 2)
-	{
+	    // if the user selects the linked list prompt
+		if (userSelection == linkedListSelection)
+		{
+
+			// Task 2.1: Prompt the user for a linked list size
+
+				// prompt the user how many digits they would like in their linked list
+				printf("How many digits would you like to enter into your linked list? \n");
+
+				// store that user input for the linked list size in linkedListSize
+				scanf_s("%d", &linkedListSize);
+
+			// Task 2.2: Add nodes to the linked list
+
+				// loop through however big the size of the linked list is
+				for (index = 0; index < linkedListSize; index++)
+				{
+					// add a new node to the linked list 
+					node = AddNode(node);
+				}
+
+			// Task 2.3: Display the linked list
+
+				// display the linked list
+				DisplayLinkedList(node);
+
+			// Task 2.4: Sort the linked list
 
 
 
-		// prompt user for digits to place into linked list
-		node = AddNode(node);
 
-
-		// display the linked list
-		DisplayLinkedList(node);
-
-
-
-	}
+		}
 
 
 
