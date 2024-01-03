bd2c46a472521b9a7fd58ff09f705cdc688983f9
chasespigarelli21 <cspigarelli9@gmail.com>
2024-01-03 01:41:55 -0700

    threads starting

--- TheLearningProject/Threads.cpp
+++ TheLearningProject/Threads.cpp
@@ -1,26 +1,22 @@
-
-
-#include <stdio.h>
-
-
-
-
+#include <iostream>
+#include <thread>
+#include <mutex>
 
 // function that increments a count when executing
-// uses mutex lock or semaphore lock
+// uses mutex lock
 // returns the value
-int IncrementValueInThread(int value)
+int IncrementValueInThread(int& value, std::mutex& mtx)
 {
-	
-	value++;
+    std::lock_guard<std::mutex> lock(mtx);
+    value++;
+    return value;
+}
 
-	return value;
+int DecrementValueInThread(int& value, std::mutex& mtx)
+{
+    std::lock_guard<std::mutex> lock(mtx);
+    value--;
+    return value;
 }
 
 
-int DecrementValueInThread(int value)
-{
-	value--;
-
-	return value;
-}
